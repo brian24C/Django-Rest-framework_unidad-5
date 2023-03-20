e8f400f678c1fded0b4e59d71fc12ded16a37759
Brian Castro <109192347+brian24C@users.noreply.github.com>
2023-03-20 15:40:50 -0500

    update CSRF_TRUSTED_ORIGINS

--- myproject/settings.py
+++ myproject/settings.py
@@ -50,7 +50,7 @@ INSTALLED_APPS = [
 ]
 SITE_ID = 1
 
-CSRF_TRUSTED_ORIGINS = ['https://web-production-f5824.up.railway.app']
+CSRF_TRUSTED_ORIGINS = ['https://web-production-e97fa.up.railway.app']
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
